59edf6d372314e456404386b77a2633029fb5dc9
Phronesis-Construction <148561117+phronesis-construction@users.noreply.github.com>
2023-10-27 08:05:19 -0500

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'Phronesis Construction Company Ltd.';
+$email_from = 'info@phronesis-construction.github.io/homes/';
 
 $email_subject = 'New Comment Submission'
 
